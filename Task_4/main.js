ce2f86253191431d39dd0c83ff2b350e2e78e0c1
tarastm95 <tarasmazepa95@gmail.com>
2024-05-10 01:03:16 +0300

    JS_H_W_2024_Task_4

--- Task_4/main.js
+++ Task_4/main.js
@@ -170,6 +170,8 @@ function swap(arr, elem_1, elem_2) {
 }
 
 console.log(swap(swap_array, 1, 0));
+
+console.log(`=========================== Завдання номер 13 =============================`);
 // 13 - - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250;
 
@@ -181,3 +183,5 @@ function exchange (sumUAH,currencyValues,exchangeCurrency) {
     }
 }
 console.log(exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD'));
+
+console.log(`=========================== THE END =============================`);
